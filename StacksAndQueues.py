34040ffcf8d82f49c25b97169de5edd49f27d726
Najlaa Nawaii <50683040+najlaahassabo@users.noreply.github.com>
2020-05-20 08:18:50 +0300

    Add files via upload

---
+++ StacksAndQueues.py
@@ -0,0 +1,26 @@
+#STACKS
+
+stack=[] #it's just a LIST!
+stack.append(1)
+stack.append(3)
+stack.append(5)
+print(stack)
+stack.pop()
+print(stack)
+print(stack[-1]) #top most
+stack.pop()
+#stack.pop()
+#if not stack :
+#    print("it's empty")
+
+#
+##QUEUES
+#queue=[] #it's just a LIST!
+#queue.append(1)
+#queue.append(3)
+#queue.append(5)
+#print(queue)
+#queue.pop(0)
+#print(queue)
+#print(queue[-1]) #top most
+#queue.pop(0)
